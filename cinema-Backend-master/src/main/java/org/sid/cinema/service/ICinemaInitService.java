2c731127cc8599e79d363615adebaf7db1a78f32
younes863 <127497127+younes863@users.noreply.github.com>
2023-06-11 13:18:55 +0100

    Add files via upload

---
+++ cinema-Backend-master/src/main/java/org/sid/cinema/service/ICinemaInitService.java
@@ -0,0 +1,14 @@
+package org.sid.cinema.service;
+
+public interface ICinemaInitService {
+	public void initVilles();
+	public void initCinemas();
+	public void initSalles();
+	public void initPlaces();
+	public void initSeance();
+	public void initCategories();
+	public void initFilm();
+	public void initProjections();
+	public void initTickets();
+
+}
